9d1886904ee2e081415367ecf096c259d058ce6b
Vested-eel <119294957+Vested-eel@users.noreply.github.com>
2026-07-29 16:02:33 +0800

    Added function for Understanding Components & Props

--- Milestone_5/react-tailwind-demo/src/App.jsx
+++ Milestone_5/react-tailwind-demo/src/App.jsx
@@ -1,6 +1,20 @@
+import HelloWorld from "./HelloWorld";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">Tailwind is working!</h1>
+    <div>
+      <HelloWorld name="Focus Bear" />
+    </div>
   );
 }
+
 export default App;
+
+// ## For Setting up a React project section
+
+// function App() {
+//   return (
+//     <h1 className="text-3xl font-bold text-blue-500">Tailwind is working!</h1>
+//   );
+// }
+// export default App;
